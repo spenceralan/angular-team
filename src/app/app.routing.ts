7762b80d22d310d74f22ec9ea8083c485aef0570
Spencer Alan <spencer.alan@icloud.com>
2017-06-16 12:25:18 -0700

    connect member create to router

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,16 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { MemberCreateComponent } from './member-create/member-create.component'
 
 const appRoutes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'new-member',
+    component: MemberCreateComponent,
   },
 ];
 
