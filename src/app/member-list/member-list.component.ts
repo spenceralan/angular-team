59bd90d1814eb297418237707a79e0d7a82c5fc1
Spencer Alan <spencer.alan@icloud.com>
2017-06-16 13:39:12 -0700

    display members on home page

--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../firebase.service';
+import { Member } from '../member.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+  styleUrls: ['./member-list.component.css'],
+  providers: [FirebaseService],
 })
 export class MemberListComponent implements OnInit {
 
-  constructor() { }
+  members;
+
+  constructor(private firebaseService: FirebaseService, private router: Router) { }
 
   ngOnInit() {
+    this.members = this.firebaseService.getMembers();
   }
+
+
 
 }
