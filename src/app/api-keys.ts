25e6bf4df475e0d81904de3f1cb9594a074e4ef9
Spencer Alan <spencer.alan@icloud.com>
2017-06-16 11:37:43 -0700

    git ignore updated

--- src/app/api-keys.ts
+++
@@ -1,8 +0,0 @@
-export const masterFirebaseConfig = {
-    apiKey: "AIzaSyAw2ZgGPxvWPdOYri1XSknx7RvNKsoyLVk",
-    authDomain: "swim-team-30874.firebaseapp.com",
-    databaseURL: "https://swim-team-30874.firebaseio.com",
-    projectId: "swim-team-30874",
-    storageBucket: "",
-    messagingSenderId: "258124040291"
-  };
